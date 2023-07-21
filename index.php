30437d2e97d56e998f63b761c4d0a162bc2dcc2d
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-21 15:49:48 +0200

    definitive edition (no bonus)

--- index.php
+++ index.php
@@ -21,3 +21,14 @@ class Movie
         return "Titolo: {$this->title}, Genere: {$this->genre}, Anno: {$this->year}";
     }
 }
+
+
+// Istanziazione dei due oggetti Movie
+$movie1 = new Movie("Il Signore degli Anelli: La Compagnia dell'Anello", "Fantasy", 2001);
+$movie2 = new Movie("Il Signore degli Anelli: Le due torri", "Fantasy", 2002);
+$movie3 = new Movie("Il Signore degli Anelli: Il ritorno del re", "Fantasy", 2003);
+
+// Stampa delle informazioni sugli oggetti Movie
+echo $movie1->getMovieInfo() . "<br>";
+echo $movie2->getMovieInfo() . "<br>";
+echo $movie3->getMovieInfo() . "<br>";
